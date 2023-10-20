1d0cbb51b1b27e5c0bd71f17b91257f271026aad
Nataliya Statova <natasha_pupkina@mail.ru>
2023-10-20 15:53:32 +0300

    Done!

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,7 +1,8 @@
 package app.model;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Timer {
-
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
